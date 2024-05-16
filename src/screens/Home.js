ba88f0569fc5fba361470a6ded1f85fcae28c59a
ElisaSPereira <pereiraelisa365@gmail.com>
2024-05-16 10:06:58 -0300

    atualização perfil

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useAuth } from "../context/useAuth";
 
-export  default function Home() {
-    return(
-        <View style={{ 
-            justifyContent: "center", 
-            alignItems:"center",
+export default function Home() {
+    const { signOut } = useAuth()
+    return (
+        <View style={{
+            justifyContent: "center",
+            alignItems: "center",
         }}>
             <Text>Home</Text>
+            <TouchableOpacity onPress={() => signOut()}>
+                <Text>Sair</Text>
+            </TouchableOpacity>
         </View>
     )
 }
